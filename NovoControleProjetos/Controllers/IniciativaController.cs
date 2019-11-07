b587b978f67dff7d563478e631978f128038d72d
Alessandro Bernardo <alessandro_bernardo@outlook.com.br>
2019-11-07 19:42:18 -0200

    controller view iniciativa create

---
+++ NovoControleProjetos/Controllers/IniciativaController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NovoControleProjetos.Controllers
+{
+    public class IniciativaController : Controller
+    {
+        // GET: Iniciativa
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
