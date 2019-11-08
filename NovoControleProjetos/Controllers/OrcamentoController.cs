e50a18bb4aba671811b179dcc954e7f7a4c903b1
Alessandro Bernardo <alessandro_bernardo@outlook.com.br>
2019-11-08 18:37:53 -0200

    Orcamento controller, dal, model, view e renderizando crua na create iniciativa

---
+++ NovoControleProjetos/Controllers/OrcamentoController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NovoControleProjetos.Controllers
+{
+    public class OrcamentoController : Controller
+    {
+        // GET: Orcamento
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult _Create()
+        {
+            return PartialView();
+        }
+
+        public ActionResult _Details(int cod_orcamento)
+        {
+            return PartialView();
+        }
+
+        public ActionResult _Edit(int cod_orcamento)
+        {
+            return PartialView();
+        }
+
+    }
+}
---
+++ NovoControleProjetos/DAL/Orcamento_DAL.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NovoControleProjetos.DAL
+{
+    public class Orcamento_DAL
+    {
+    }
+}
