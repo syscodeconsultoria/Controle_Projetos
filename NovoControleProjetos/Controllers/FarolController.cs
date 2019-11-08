3c987fafc540cfee58bf103c87b96c09900e5b95
Willians <willians.frutuoso@syscodeconsultoria.com.br>
2019-11-08 16:58:02 -0300

    Farol

---
+++ NovoControleProjetos/Controllers/FarolController.cs
@@ -0,0 +1,22 @@
+﻿using NovoControleProjetos.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NovoControleProjetos.Controllers
+{
+    public class FarolController : Controller
+    {
+        private Farol_DAL dbFarol = new Farol_DAL();
+
+        // GET: Farol
+        public ActionResult _ListaFarol()
+        {
+            var farois = dbFarol._ListaFarol();
+            ViewBag.farois = farois;
+            return PartialView(farois);
+        }
+    }
+}
---
+++ NovoControleProjetos/DAL/Farol_DAL.cs
@@ -0,0 +1,37 @@
+﻿using NovoControleProjetos.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace NovoControleProjetos.DAL
+{
+    public class Farol_DAL
+    {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["NovoControleProjetos"].ConnectionString);
+
+        public List<Farol> _ListaFarol()
+        {
+            var farois = new List<Farol>();
+            var cmd = new SqlCommand("producao.UP_Controle_Projetos_Lista_Farois", con);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            con.Open();
+            var reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                farois.Add(new Farol
+                {
+                    IdFarol = Convert.ToInt32(reader["id_farol"]),
+                    Ds_Farol = reader["ds_farol"] as string,
+                    Cor_Farol = reader["cor_farol"] as string
+                });
+            }
+            reader.Close();
+            con.Close();
+            return farois;
+        }
+    }
+}
