c91251500188aaaaadeb418c1ea638282212795b
Alessandro Bernardo <alessandro_bernardo@outlook.com.br>
2019-11-08 17:32:59 -0200

    controller, dal, model, view {esteira} (já coloquei na view Create no campo de esteira).

---
+++ NovoControleProjetos/DAL/Esteira_DAL.cs
@@ -0,0 +1,40 @@
+﻿using NovoControleProjetos.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace NovoControleProjetos.DAL
+{
+    public class Esteira_DAL
+    {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["NovoControleProjetos"].ConnectionString);
+
+        public List<Esteira> ListaEsteira()
+        {
+            List<Esteira> esteiras = new List<Esteira>();
+
+            SqlCommand command = new SqlCommand("producao.UP_Controle_Projetos_Lista_Esteiras", con);
+            command.CommandType = CommandType.StoredProcedure;
+
+            con.Open();
+            using (SqlDataReader sdr = command.ExecuteReader())
+            {
+                while (sdr.Read())
+                {
+                    esteiras.Add(new Esteira
+                    {
+                        Id_Esteira = Convert.ToInt32(sdr["cod_esteira"]),
+                        Ds_Esteira = sdr["ds_esteira"] as string                        
+                    });
+                }
+            }
+            con.Close();
+
+            return esteiras;
+        }
+    }
+}
---
+++ NovoControleProjetos/Models/Esteira.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NovoControleProjetos.Models
+{
+    public class Esteira
+    {
+        public int Id_Esteira{ get; set; }
+        public string Ds_Esteira { get; set; }
+    }
+}
