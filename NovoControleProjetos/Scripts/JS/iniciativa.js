2978f7001952e7b04b4f227895b627eee16bc54f
Willians <willians.frutuoso@syscodeconsultoria.com.br>
2019-11-11 14:54:45 -0300

    cores farol

---
+++ NovoControleProjetos/Scripts/JS/iniciativa.js
@@ -0,0 +1,26 @@
+﻿$(document).ready(function () {
+
+    
+
+    $("#farol").change(function () {
+        
+        var background = $("#farol option:selected").val().split(' ')[1];
+        var font = "";
+        var title = $("#farol option:selected").text();
+
+        if (background === 'yellow' || background === 'white') {
+            font = 'black';
+        } else if (title === "") {
+            background = 'white';
+            
+        }
+        else {
+            font = 'white';
+        }
+        $(this).css("background", background).css("color", font);
+        $("#farol option").css("background", "white").css("color", "black");
+    });
+
+   
+
+})
