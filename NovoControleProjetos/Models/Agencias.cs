4c9aa4c6a4655ea3619256d3b9d13fcd3d114174
Alessandro Bernardo <alessandro_bernardo@outlook.com.br>
2019-12-10 14:00:48 -0200

    ajuste no nome da agencia visitada para trazer o código concatenado

--- NovoControleProjetos/Models/Agencias.cs
+++ NovoControleProjetos/Models/Agencias.cs
@@ -11,5 +11,14 @@ namespace NovoControleProjetos.Models
         public int? cod_agencia { get; set; }
         public DateTime? data_visita { get; set; }
         public string nome_agencia { get; set; }
+
+        public string codigo_e_agencia
+        {
+            get
+            {
+                return string.Format("{0} {1} {2}", id_agencia, " - ", nome_agencia);
+            }
+
+        }
     }
 }
